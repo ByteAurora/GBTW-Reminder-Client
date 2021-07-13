cfd72888f6ad279ee33b849b4359e55bf3ac7975
Sohn Young Jin <sonkim1001@naver.com>
2021-07-13 19:42:18 +0900

    Create base logic for notification
    Add gbtw logo

--- app/src/main/java/com/onedeveloper/gbtw_reminder/GbtwFirebaseMessagingService.java
+++ app/src/main/java/com/onedeveloper/gbtw_reminder/GbtwFirebaseMessagingService.java
@@ -1,66 +1,79 @@
 package com.onedeveloper.gbtw_reminder;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class GbtwFirebaseMessagingService {
-    public class GbtfFirebaseMessagingService extends FirebaseMessagingService {
-        @Override
-        public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-            if (remoteMessage.getData().size() > 0) {
-                if (/* Check if data needs to be processed by long running job */ true) {
-                    // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                    // scheduleJob();
-                } else {
-                    // Handle message within 10 seconds
-                    //handleNow();
-                }
+// Test device token: d1VRBeqsSJiyR6djvMqrNH:APA91bHojG43VZL3M9PTp6T_RLLWpcQocPBLB4_Rog_7cQuN1sF8-3t0jTTsD-qI16PSu5HbILXFBg6eY7TFP5dCpYdS4Y4tWehcRYgN2z29-5NU64KuiNqtt99gBGAeuTkVwaOfMJ62
+
+public class GbtwFirebaseMessagingService extends FirebaseMessagingService {
+    private final static String GBTW_NOTIFICATION_GROUP = "GBTW NOTIFICATION GROUP";
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("GBTW LOG", "Data: " + remoteMessage.getData());
+        } else if (remoteMessage.getNotification() != null) {
+            String title = remoteMessage.getNotification().getTitle();
+            String message = remoteMessage.getNotification().getBody();
+            Log.d("GBTW LOG", "Notification: {Title: " + title + ", Message: " + message + "}");
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 153, intent, PendingIntent.FLAG_ONE_SHOT);
+            String channelId = "GBTWLOG";
+
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channelId)
+                    .setGroup(GBTW_NOTIFICATION_GROUP)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                    .setContentIntent(pendingIntent);
+
+            NotificationCompat.Builder notificationGroup = new NotificationCompat.Builder(this, channelId)
+                    .setContentTitle(title)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setGroup(GBTW_NOTIFICATION_GROUP)
+                    .setGroupSummary(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            SharedPreferences sharedPreferences = getSharedPreferences("SP_GBTW", MODE_PRIVATE);
+            int notificationSequence = sharedPreferences.getInt("LAST_NOTIFICATION_SEQUENCE", 0) + 1;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                String channelName = "GBTW";
+                NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(notificationChannel);
+                notification.setChannelId(channelId);
             }
 
-            if (remoteMessage.getNotification() != null) {
-                String title = remoteMessage.getNotification().getTitle();
-                String message = remoteMessage.getNotification().getBody();
-                Log.d("GBTW", "Title: " + title + ", Message: " + message);
-
-                Intent intent = new Intent(this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-                String channelId = "GBTW";
-                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle(title)
-                        .setContentText(message)
-                        .setAutoCancel(true)
-                        .setContentIntent(pendingIntent);
-                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    String channelName = "GBTW2";
-                    NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-                    notificationManager.createNotificationChannel(notificationChannel);
-                }
-                notificationManager.notify(0, notificationBuilder.build());
-            }else{
-                Log.d("GBTW", "Receive data failed");
-            }
+            notificationManager.notify(notificationSequence, notification.build());
+            notificationManager.notify(0, notificationGroup.build());
+            sharedPreferences.edit().putInt("LAST_NOTIFICATION_SEQUENCE", notificationSequence).apply();
+        } else {
+            Log.d("GBTWLOG", "Notification: Receive data failed : " + remoteMessage.getData());
         }
+    }
 
-        @Override
-        public void onNewToken(@NonNull String s) {
-            super.onNewToken(s);
-            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-            Log.d("GBTW", "알림 메시지: 새로운 토큰 생성");
-        }
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.d("GBTWLOG", "Create New Token: Success");
     }
 }
--- app/src/main/java/com/onedeveloper/gbtw_reminder/MainActivity.java
+++ app/src/main/java/com/onedeveloper/gbtw_reminder/MainActivity.java
@@ -1,8 +1,15 @@
 package com.onedeveloper.gbtw_reminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.w("GBTWLOG", "Fetching FCM registration token failed", task.getException());
+                        return;
+                    }
+
+                    // Get new FCM registration token
+                    String token = task.getResult();
+
+                    // Log and toast
+                    Log.d("GBTWLOG", "Application Token: " + token);
+                });
+
+        FirebaseMessaging.getInstance().subscribeToTopic("GBTF")
+                .addOnCompleteListener(task -> {
+                    String msg = "Subscribe Result: success";
+                    if (!task.isSuccessful()) {
+                        msg = "Subscribe Result: fail";
+                    }
+                    Log.d("GBTWLOG", msg);
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                });
     }
 }
