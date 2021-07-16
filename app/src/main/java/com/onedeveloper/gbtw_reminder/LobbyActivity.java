5ed0281ee0ef8547c02e38e30307c322a158cfcf
Sohn Young Jin <sonkim1001@naver.com>
2021-07-16 19:52:06 +0900

    Update LobbyActivity - Add title bar

--- app/src/main/java/com/onedeveloper/gbtw_reminder/LobbyActivity.java
+++ app/src/main/java/com/onedeveloper/gbtw_reminder/LobbyActivity.java
@@ -2,13 +2,30 @@ package com.onedeveloper.gbtw_reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LobbyActivity extends AppCompatActivity {
+    private ImageButton btnTitleBarOpenDrawer;
+    private ImageButton btnTitleBarPushSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        init();
+    }
+
+    private void init(){
+        btnTitleBarOpenDrawer = findViewById(R.id.btn_titlebar_opendrawer);
+        btnTitleBarPushSetting = findViewById(R.id.btn_titlebar_pushsetting);
+
+        btnTitleBarPushSetting.setOnClickListener((v) -> {
+            Intent intent = new Intent(LobbyActivity.this, PushSettingActivity.class);
+            startActivity(intent);
+        });
     }
 }
